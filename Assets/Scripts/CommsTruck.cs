15e6aee856d97387846901703a8c300058af3ca1
Redex <marin.petric73@gmail.com>
2023-05-25 03:03:33 +0200

    Added explosion to comms truck

---
+++ Assets/Scripts/CommsTruck.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(BNG.Damageable))]
+public class CommsTruck : MonoBehaviour
+{
+    [SerializeField] GameObject smallFire;
+    [SerializeField] GameObject largeFire;
+    [SerializeField] GameObject explosion;
+    private BNG.Damageable damageable;
+    private float maxHp;
+
+    void Start()
+    {
+        damageable = GetComponent<BNG.Damageable>();
+        maxHp = damageable.Health;
+    }
+
+    public void OnDamage(float damage)
+    {
+        if(damageable.Health / maxHp < 0.75)
+            smallFire.SetActive(true);
+        if(damageable.Health / maxHp < 0.40)
+            largeFire.SetActive(true);
+    }
+
+    public void OnDestruction()
+    {
+        Instantiate(explosion, transform.position, Quaternion.identity);
+    }
+}
---
+++ Assets/Scripts/Explosion.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Explosion : MonoBehaviour
+{
+    void Start()
+    {
+        float maxLen = Mathf.Max(GetComponent<ParticleSystem>().main.duration, GetComponent<AudioSource>().clip.length);
+        Invoke("DestroySelf", maxLen);
+    }
+    
+    void Destroy()
+    {
+        GameObject.Destroy(this.gameObject);
+    }
+}
